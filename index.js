433ec3e5ea5b2bd6f10aea6da900e1d93a876f7a
Taanveer22 <taanveer469@gmail.com>
2026-05-19 03:19:47 +0600

    alhamdulliah jwt login and logout route implemented done

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 
 // database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
@@ -48,6 +53,15 @@ async function run() {
 
       res
         .cookie('token', tokenValue, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post('/jwt/logout', (req, res) => {
+      res
+        .clearCookie('token', {
           httpOnly: true,
           secure: false,
         })
