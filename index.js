ffd8452821650eb879493491472647b6863ec18d
Taanveer22 <taanveer469@gmail.com>
2026-05-21 04:38:40 +0600

    alhamdulliah remove cookie options

--- index.js
+++ index.js
@@ -92,7 +92,6 @@ async function run() {
           httpOnly: true,
           secure: true,
           sameSite: 'none',
-          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
         })
         .send({ success: true });
     });
