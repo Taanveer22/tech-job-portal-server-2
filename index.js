cf8099594335beb7f91dc2a7c3dc71f130647596
Taanveer22 <taanveer469@gmail.com>
2026-05-23 13:52:19 +0600

    alhamdulliah sever related issue solved

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// ======================================================
+// PACKAGES
+// ======================================================
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
